360f9a0181f40be1523c39cde91cbd8a821e9203
haianhxanh <hanka7n@gmail.com>
2022-11-07 16:22:28 +0100

    Q object

--- python-django-with-mosh/python-django/storefront/playground/views.py
+++ python-django-with-mosh/python-django/storefront/playground/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from store.models import Product
 # return HttpResponse('Hello World')
 # instead of returning the whole HttpResponse we will render the html from our templates
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Hanka'})
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+
+
+    return render(request, 'hello.html', {'name': 'Hanka', 'products': list(queryset)})

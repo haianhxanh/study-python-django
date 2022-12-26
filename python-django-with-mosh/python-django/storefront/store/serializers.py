c6d547b7d6366cdbb23a1088a9c4897612646561
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2022-12-26 16:54:04 +0100

    Create order items

--- python-django-with-mosh/python-django/storefront/store/serializers.py
+++ python-django-with-mosh/python-django/storefront/store/serializers.py
@@ -1,8 +1,5 @@
-import collections
-from black.brackets import max_delimiter_priority_in_atom
-from django.db.models import Count
+from django.db import transaction
 from rest_framework import serializers
-import rest_framework.decorators
 from store.models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
 from decimal import Decimal
 
@@ -150,8 +147,22 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data["cart_id"])
-        print(self.context["user_id"])
+        with transaction.atomic():
 
-        (customer, created) = Customer.objects.get_or_create(user_id=self.context["user_id"])
-        Order.objects.create(customer=customer)
+            cart_id = self.validated_data["cart_id"]
+            (customer, created) = Customer.objects.get_or_create(user_id=self.context["user_id"])
+            order = Order.objects.create(customer=customer)
+
+            cart_items = CartItem.objects.select_related("product").filter(cart_id=cart_id)
+
+            # list comprehension - for each item in query set create an order item
+            order_items = [
+                OrderItem(
+                    order=order, product=item.product, unit_price=item.product.unit_price, quantity=item.quantity
+                )
+                for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+
+            # delete shopping cart
+            Cart.objects.filter(pk=cart_id).delete()

4f04553a0d4bbd6fc32d1d6c26d567135c3e9012
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2022-12-26 23:52:17 +0100

    Validating uploaded files

--- python-django-with-mosh/python-django/storefront/store/models.py
+++ python-django-with-mosh/python-django/storefront/store/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib import admin
 import uuid
 from django.conf import settings
+from store.validators import validate_file_size
 from storefront.settings import AUTH_USER_MODEL
 
 
@@ -41,7 +42,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to="store/images")
+    image = models.ImageField(upload_to="store/images", validators=[validate_file_size])
 
 
 class Customer(models.Model):

d7175e6e54b0a5f5ec155ca632d9e72f2fce6ac1
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2022-12-31 12:40:03 +0100

    BaseEmailMessage

--- python-django-with-mosh/python-django/storefront/playground/views.py
+++ python-django-with-mosh/python-django/storefront/playground/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
+from templated_mail.mail import BaseEmailMessage
 from django.db.models import Q, F, Count, DecimalField, ExpressionWrapper, Func, Value
 from django.db.models.aggregates import Max, Min, Avg, Sum
 from django.db.models.functions import Concat
@@ -17,11 +18,16 @@ def say_hello(request):
     try:
         # send_mail("subject", "message", "hankawork@gmail.com", ["hanka7n@gmail.com", "hankawork@gmail.com"])
         # mail_admins("subject", "message", html_message="message")
-        message = EmailMessage(
-            "subject", "message", "hankawork@gmail.com", ["hanka7n@gmail.com", "hankawork@gmail.com"]
+        # message = EmailMessage(
+        #     "subject", "message", "hankawork@gmail.com", ["hanka7n@gmail.com", "hankawork@gmail.com"]
+        # )
+        # message.attach_file("playground/static/images/blake-yuto-VqxNK0FpSPo-unsplash.jpg")
+        # message.send()
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Hanka'}
         )
-        message.attach_file("playground/static/images/blake-yuto-VqxNK0FpSPo-unsplash.jpg")
-        message.send()
+        message.send(['hankawork@gmail.com'])
     except BadHeaderError:
         pass  # e.g. Email successfully sent
     return render(request, "hello.html", {"name": "Hanka"})

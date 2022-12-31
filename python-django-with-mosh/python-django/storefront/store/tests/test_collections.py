5df90bf47f0e297bc471a89a2a99532ba41c9708
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2022-12-31 17:28:49 +0100

    Tasks

--- python-django-with-mosh/python-django/storefront/store/tests/test_collections.py
+++ python-django-with-mosh/python-django/storefront/store/tests/test_collections.py
@@ -1,10 +1,37 @@
+import pytest
 from rest_framework.test import APIClient
 from rest_framework import status
+from django.contrib.auth.models import User
 
 
+@pytest.mark.django_db
 class TestCreateCollection:
+    # @pytest.mark.skip
     def test_if_user_is_anonymous_returns_401(self):
         client = APIClient()
-        response = client.post("/store/collections/", {"title": "a"})
+        response = client.post("/store/collections/", {"title": "beauty"})
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post("/store/collections/", {"title": "beauty"})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post("/store/collections/", {"title": "beauty"})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data["title"] is not None
+
+    def test_if_data_is_valid_returns_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post("/store/collections/", {"title": "beauty"})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["id"] > 0
